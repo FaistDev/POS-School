3abdde461dcd5043569f86a0cccb83be17755732
ben-lap <ben@faistdev.at>
2019-11-13 21:16:22 +0100

    basic database class

---
+++ OnlineShop/src/main/java/at/faistdev/onlineshop/data/Database.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.faistdev.onlineshop.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Ben
+ */
+public class Database {
+    private static Database instance;
+    private final Connection connection;
+    
+    private final String dbName = "";
+    private final String dbUsername = "postgres";
+    private final String dbPassword = "postgres";
+    
+    public static Database getInstance() throws SQLException{
+        if(instance==null){
+            instance = new Database();
+        }
+        return instance;
+    }
+    
+    private Database() throws SQLException{
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost/"+dbName, dbUsername, dbPassword);
+    }
+}
